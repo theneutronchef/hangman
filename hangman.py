fcb6f3c76af14ba0a87fdb637b418a6664225679
Gabriel Tan <tanjunxian@gmail.com>
2015-01-11 00:37:49 +0900

    added ascii

--- hangman.py
+++ hangman.py
@@ -2,6 +2,22 @@ import math
 import random
 
 f = open("words.txt", "r")
+chars = []
+for i in range(8):
+	chars.append(" ")
+chars_lib = ["|", "0", "| |", "/", "\\", "A", "/", "\\"]
+
+def create_image():
+	return """
+ _______
+|   """ + chars[0] + """
+|   """ + chars[1] + """
+| """ + chars[3] + chars[2] + chars[4] + """
+|   """ + chars[5] + """
+|  """ + chars[6] + " " + chars[7] + """ 
+|
+|_______
+""" 
 
 word_dict = []
 
@@ -20,35 +36,35 @@ def check_word(word_list, letter):
 
 def blanks(word):
 	l = []
-	for i in range(len(word)):
-		l.append('_')
+	for i in range(len(word)-1):
+		l.append('_ ')
 	return l
 
 def run():
 	running = True
-	index = math.floor(len(word_dict) * random.random())
+	index = int(math.floor(len(word_dict) * random.random()))
 	word = word_dict[index]
 	orig_list = list(word)
 	curr_list = blanks(word)
-	lives = 10
-	print("Number of lives: " + str(lives))
+	count = 0
 	while running:	
-		print(curr_list)
+		print("".join(curr_list))
 		letter = input("Input letter: ")
 		indices = check_word(orig_list, letter)
 		if len(indices) == 0:
-			lives = lives - 1
+			chars[count] = chars_lib[count]
+			count = count + 1
 			print(letter + " does not exist")
-			print("Lives remaining: " + str(lives))
+			print(create_image())
 		else:
 			for i in indices:
-				curr_list[i] = letter
-		if '_' not in curr_list:
+				curr_list[i] = letter + " "
+		if '_ ' not in curr_list:
 			running = False
 			print("You win!")
-		elif lives == 0:
+		elif count == 8:
 			running = False
-			print("You ran out of lives!")
+			print("You died!")
 			print("Answer: " + word)
 
 run()
